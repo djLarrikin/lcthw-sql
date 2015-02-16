55593cb615585f0b2b240df6c851752c45bc60e4
Jonathan Morton <djlarrikin@gmail.com>
2015-02-16 20:08:23 +0900

    Exercise 11 completed

---
+++ ex11.sql
@@ -0,0 +1,12 @@
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (0, "Frank", "Smith", 100);
+
+INSERT OR REPLACE INTO person (id, first_name, last_name, age)
+	VALUES (0, "Frank", "Smith", 100);
+
+SELECT * FROM person;
+
+REPLACE INTO person (id, first_name, last_name, age)
+	VALUES (0, "Jonathan", "Morton", 26);
+
+SELECT * FROM person;
